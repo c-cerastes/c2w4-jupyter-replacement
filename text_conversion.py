a6ebeae19e53cd4ae56749f41fa3378bc3b70b9f
Cerastes <97221598+c-cerastes@users.noreply.github.com>
2022-01-07 11:19:24 -0500

    python file addition

---
+++ text_conversion.py
@@ -0,0 +1,15 @@
+import random, re
+
+def troutspeak(inputstring):
+    lis = []
+    x=1
+    for i in inputstring:
+        x^=i.isalpha()
+        lis.append((2*i).title()[x])
+    return "".join(lis)
+
+text = input("Please enter text:")
+
+text = troutspeak(text)
+
+print(text)
